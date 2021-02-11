08dcdb168fa63786ea97a7b1fc897c5024a83d7f
Danny Rivera <samboard@hotmail.com>
2021-02-11 10:56:26 -0500

    commit tree 8d55fc88e0a70c69a332c001e42e2fca43c4cf6e

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
+require 'pry'
 # code here!
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def name
+        @name
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student_name, grade)
+        if @roster[grade] == nil
+            @roster[grade] = [student_name]
+        else
+            @roster[grade] << student_name
+        end
+    end
+
+    def grade(grade_number)
+        @roster[grade_number]
+    end
+
+    def sort
+        @returning_hash = {}
+        @roster.each do |key, value|
+            @returning_hash[key] = value.sort
+        end
+        @returning_hash
+    end
+end
+
+binding.pry
